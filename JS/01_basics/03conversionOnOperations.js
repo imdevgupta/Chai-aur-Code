2e3e8d930467d13c1bedfc23e86f773cc3a64108
Dev <guptadev531@gmail.com>
2026-01-28 16:03:27 +0530

    Conversion and Confusion in this

---
+++ JS/01_basics/03conversionOnOperations.js
@@ -0,0 +1,27 @@
+let score = "abc";
+
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+// "33" => 33
+// "33ab" => NaN type is number
+// true => 1
+// false => 1
+// null => NaN
+// undefined = > NaN
+
+let loogedIn = "Dev";
+
+let booleanLoggedIn = Boolean(loogedIn);
+
+console.log(booleanLoggedIn);
+
+// 1 => true
+// 0 => false
+// "" => false 
+// " " or "Dev" => true as space also have value 
