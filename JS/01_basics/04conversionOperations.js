5e6358dba3a55bb5f989c475cb45e5a4355dc75f
Dev <guptadev531@gmail.com>
2026-01-29 15:16:22 +0530

    Type Coresion and Operations

---
+++ JS/01_basics/04conversionOperations.js
@@ -0,0 +1,19 @@
+let someNumber = 23;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+let str1 = "Hello";
+let str2 = " Dev!";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
